c1f098ab7abcb79cebdbafd01baff06e641ff2ae
vuilfanie <55847853+vuilfanie@users.noreply.github.com>
2023-01-27 19:22:23 +0200

    add/pages
    Added login page
    Added Styled Components
    Setup React router
    Setup Smoothscrolling app

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,74 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { accessToken, logout, getCurrentUserProfile } from './Spotify';
+import { catchErrors } from './utils';
+import SmoothScroll from './SmoothScroll';
+import { GlobalStyle } from './styles';
+import { Login } from './pages';
+
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
+    setToken(accessToken);
 
-    console.log(accessToken);
-    console.log(refreshToken);
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
+    const fetchData = async () => {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
     }
-  })
+
+    catchErrors(fetchData());
+
+  }, []);
 
   return (
     <div className="App">
+      <GlobalStyle />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-        >
-          Login
-        </a>
+      {!token ? (
+        <Login />
+        ) : (
+          <Router>
+            <SmoothScroll>
+              <Routes>
+                <Route path="/top-artists" element={<h1>Top Artists</h1>}>
+                  
+                </Route>
+                <Route path="/top-tracks" element={<h1>Top Tracks</h1>}>
+                  
+                </Route>
+                <Route path="/playlists/:id" element={<h1>Playlist</h1>}>
+                  
+                </Route>
+                <Route path="/playlists" element={<h1>Playlists</h1>}>
+                  
+                </Route>
+                <Route path="/" element={<></>}>
+                  
+                </Route>
+              </Routes>
+            </SmoothScroll>
+          </Router>
+        )}
+        <>
+          <button onClick={logout}>Log Out</button>
+
+          {profile && (
+            <div>
+              <h1>{profile.display_name}</h1>
+              {profile.images.length && profile.images[0].url && (
+                <img src={profile.images[0].url} alt="Avatar"/>
+              )}
+            </div>
+          )}
+        </>
       </header>
     </div>
   );
